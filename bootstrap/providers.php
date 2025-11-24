dfa2a1ca90e91dcbe579d3d08384b8d61f6db609
Fortune4808 <opeyemiogunleye49@gmail.com>
2025-11-25 00:43:28 +0100

    I made some adjustment on the telescope to only work dev and staging stage

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,20 @@
 <?php
 
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
-];
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TelescopeServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        if ($this->app->environment('local', 'staging')) {
+            $this->app->register(\Laravel\Telescope\TelescopeApplicationServiceProvider::class);
+        }
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
