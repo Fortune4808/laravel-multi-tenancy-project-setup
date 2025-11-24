3cf4549dc13cf33228ee57b582511ce322821d5f
Fortune4808 <opeyemiogunleye49@gmail.com>
2025-11-25 00:51:50 +0100

    I commented the telescope out

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,20 +1,6 @@
 <?php
 
-namespace App\Providers;
-
-use Illuminate\Support\ServiceProvider;
-
-class TelescopeServiceProvider extends ServiceProvider
-{
-    public function register()
-    {
-        if ($this->app->environment('local', 'staging')) {
-            $this->app->register(\Laravel\Telescope\TelescopeApplicationServiceProvider::class);
-        }
-    }
-
-    public function boot()
-    {
-        //
-    }
-}
+return [
+    // App\Providers\AppServiceProvider::class,
+    // App\Providers\TelescopeServiceProvider::class,
+];
